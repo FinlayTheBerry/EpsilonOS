82a562687db77a1d0c01dad41c1c8eb9258e1e68
Finlay Christ <randomiagaming@gmail.com>
2026-06-02 14:56:36 -0700

    Much better fully automatic update script with no more sudo timeouts.

--- os_scripts/eos_updates.py
+++ os_scripts/eos_updates.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import subprocess
 import os
 import sys
@@ -38,29 +38,35 @@ def PrintError(message):
 # endregion
 
 def Main():
-    if os.geteuid() == 0 or os.getegid() == 0:
-        script_name = os.path.splitext(os.path.basename(os.path.realpath(__file__)))[0]
-        PrintError(f"{script_name} may not be run as root. Please try again.")
-        return 1
+    if os.geteuid() != 0 or os.getegid() != 0:
+        script_path = os.path.abspath(__file__)
+        return RunCommand(f"sudo \'{script_path}\'", echo=True, check=False)
     print()
 
+    if RunCommand("id yaybld", check=False) != 0:
+        RunCommand("useradd -r -U -d /var/lib/yaybld -s /usr/bin/nologin -c \'yay build user\' -M yaybld && usermod -p \'!\' yaybld")
+    if not os.path.isdir("/var/lib/yaybld"):
+        RunCommand("install -d -m 700 -o yaybld -g yaybld /var/lib/yaybld")
+
     print("\033[36mUpdating all packages...\033[0m")
-    RunCommand("yay -Syu --noconfirm", echo=True)
+    RunCommand("sudo -u yaybld yay -Syu --noconfirm", echo=True)
     print()
 
     print(f"\033[36mRemoving orphaned packages...\033[0m")
-    stdout, _ = RunCommand("yay -Qqdt", capture=True, check=False)
+    stdout, _ = RunCommand("pacman -Qqdt", capture=True, check=False)
     orphans = stdout.splitlines()
     if len(orphans) == 0:
         print("There is nothing to do.")
     else:
         print(f"{" ".join(orphans)}")
-        RunCommand(f"yay -Rns {" ".join(orphans)} --noconfirm", echo=True)
+        RunCommand(f"pacman -Rns {" ".join(orphans)} --noconfirm", echo=True)
     print()
 
     print("\033[36mClearing cache...\033[0m")
-    RunCommand("sudo rm -rf /var/cache/pacman/pkg/*", echo=True)
-    RunCommand("rm -rf ~/.cache/yay/*", echo=True)
+    RunCommand("find /var/cache/pacman/pkg -mindepth 1 -delete", check=False)
+    RunCommand("find /var/lib/yaybld -mindepth 1 -delete", check=False)
+    RunCommand("find /home/*/.cache/yay -mindepth 1 -delete", check=False)
+    RunCommand("find /root/.cache/yay -mindepth 1 -delete", check=False)
     print()
 
     return 0
